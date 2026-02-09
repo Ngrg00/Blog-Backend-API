695cdc4836e48bc6ce3a50fb7a6991c93dbc328a
Nation <nation.grg123@gmail.com>
2026-02-09 22:21:44 +0000

    Error handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectionDB = require("./config/dbConnection");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const errorHandler = require("./middleware/errorHandler.js");
 dotenv.config();
 
 connectionDB();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/", require("./routes/userRoute.js"));
 app.use("/post", require("./routes/postRoute.js"));
 app.use("/post", require("./routes/commentRoute.js"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
